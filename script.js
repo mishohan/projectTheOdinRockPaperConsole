334d34403961d36153d732df78fed971c007e6eb
mishohan <mishohan.o786@gmail.com>
2025-07-30 13:54:08 +0800

    Create > Step1 > getComputerChoice

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //Rock Paper Scissor in Console
-//
+
+
+//Step-1: Writing the logic to get Computer Choice
+
+function getComputerChoice(){
+
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+    
+    if(computerChoice === 1){
+        return "rock";
+    }
+    else if(computerChoice === 2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+
+}
+
+//console.log(getComputerChoice()); 
+// //This will return one of the: "rock", "paper", "scissor"
+getComputerChoice();
